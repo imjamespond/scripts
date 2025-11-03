69aea610d1561cff9c422b103e66da6aa9e1e83b
James <you@example.com>
2025-11-03 15:11:49 +0800

    first

---
+++ mcd/mcd.sh
@@ -0,0 +1,12 @@
+# npm run build 
+
+source $ANSIBLE_PATH/.venv/bin/activate
+cd ./ansible
+
+ansible-playbook -i inventory.ini python.yml --limit mcd-local
+
+# 不备份（默认）
+ansible-playbook -i inventory.ini deploy.yml --limit mcd -e "dir=data-model target=/srv/frontend"
+
+# 启用备份
+# ansible-playbook -i inventory.ini deploy.yml --limit mcd -e "dir=data-model target=/srv/frontend" bak=true"
---
+++ readme.md
@@ -0,0 +1,7 @@
+```bash
+# Ansible 2.10	Python 2.7 / 3.5	Python 3.9
+uv python install 3.9
+uv venv --python 3.9
+# source .venv/bin/activate
+uv pip install ansible==2.10
+```
